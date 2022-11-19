fdda426734bf1eb3cefd4be336f508c2e4d38652
kittenK531 <k.space.infinity@gmail.com>
2022-11-18 19:48:10 -0800

    hash output and pathlib done

--- main.py
+++ main.py
@@ -1,33 +1,47 @@
 import subprocess
-import numpy as np
+from pathlib import Path
+
 import matplotlib.pyplot as plt
-
-""" Calculating one orbit """
-""" Input:  parameter file 
-    Output: follow.out
-"""
-
-def get_helio_pos_vel(CWD="recom/swifter/", wdr="example"): #, outfile_name, infile_name) #TODO hash files input + new function to sed input vel and pos, Pathlib
-    # CWD = "swifter/swifter_test/"
-    # wdr = "test"
-
-    # CWD = "recom/swifter/"
-    # wdr = "example"
-
-    particle_id = 6
-    frequency = 1
-
-    subprocess.call("rm bin.dat", shell=True, cwd=CWD+wdr)
-    subprocess.call("ls -l *.out", shell=True, cwd=CWD+wdr)
-    subprocess.call("echo 'param.in' | ./../bin/swifter_tu4", shell=True, cwd=CWD+wdr)
-    subprocess.call(f"echo 'dump_param1.dat\n{particle_id}\n{frequency}\n' | ./../bin/tool_follow", shell=True, cwd=CWD+wdr)
-    subprocess.call("ls *.out", shell=True, cwd=CWD+wdr)
+import numpy as np
 
 
-    """ Plotting the trajectory """
-    f = open(CWD+wdr+"/follow.out", "r") #TODO: hash file
+def get_hash_code():
 
-    num_lines = sum(1 for line in open(CWD+wdr+"/follow.out", "r"))
+    return str(np.abs(hash(".")))[:6]
+
+
+def get_helio_pos_vel(
+    CWD="recom/swifter",
+    wdr="example",
+    outfile_name="follow.out",
+    infile_name="param.in",
+):  # , outfile_name, infile_name) #TODO hash files input + new function to sed input vel and pos
+
+    particle_id, frequency = 6, 1
+
+    cwd = Path(CWD)
+    file_dir = cwd.joinpath(Path(wdr))
+
+    outfile_hash = (
+        outfile_name.split(".")[0] + get_hash_code() + "." + outfile_name.split(".")[1]
+    )
+
+    subprocess.call("rm bin.dat *.out", shell=True, cwd=str(file_dir))
+    subprocess.call("ls -l *.out", shell=True, cwd=str(file_dir))
+    subprocess.call(
+        f"echo '{infile_name}' | ./../bin/swifter_tu4", shell=True, cwd=str(file_dir)
+    )
+    subprocess.call(
+        f"echo 'dump_param1.dat\n{particle_id}\n{frequency}\n' | ./../bin/tool_follow",
+        shell=True,
+        cwd=str(file_dir),
+    )
+    subprocess.call(f"cp {outfile_name} {outfile_hash}", shell=True, cwd=str(file_dir))
+
+    """ Getting the pos, vel via scanf """
+    f = open(file_dir.joinpath(Path(outfile_hash)), "r")  # TODO: hash file
+
+    num_lines = sum(1 for line in open(file_dir.joinpath(Path(outfile_hash)), "r"))
 
     x, y, z = np.zeros(num_lines), np.zeros(num_lines), np.zeros(num_lines)
     vx, vy, vz = np.zeros(num_lines), np.zeros(num_lines), np.zeros(num_lines)
@@ -38,7 +52,7 @@ def get_helio_pos_vel(CWD="recom/swifter/", wdr="example"): #, outfile_name, inf
 
         word_list, listlist = [], []
 
-        lines = line.split(' ')
+        lines = line.split(" ")
 
         for words in lines:
 
@@ -46,13 +60,15 @@ def get_helio_pos_vel(CWD="recom/swifter/", wdr="example"): #, outfile_name, inf
 
         for word in word_list:
 
-            splitn = word.split('\n')
+            splitn = word.split("\n")
 
-            splitn = ["-1.0"] if ('*******' in splitn) else splitn # fixing the display bug
+            splitn = (
+                ["-1.0"] if ("*******" in splitn) else splitn
+            )  # fixing the display bug
 
             listlist += [splitn[0]] if (len(splitn) > 1) else splitn
 
-        try: 
+        try:
             x[count], y[count], z[count] = listlist[2], listlist[3], listlist[4]
             vx[count], vy[count], vz[count] = listlist[5], listlist[6], listlist[7]
         except:
@@ -62,10 +78,10 @@ def get_helio_pos_vel(CWD="recom/swifter/", wdr="example"): #, outfile_name, inf
 
     return x, y, z, vx, vy, vz
 
+
 x, y, z, vx, vy, vz = get_helio_pos_vel()
 
 fig1, ax = plt.subplots()
 ax.set_box_aspect(1)
 ax.plot(x, y)
-plt.savefig('test.png')
-
+plt.savefig("test.png")
